efca87db228403c8c1ee6cb3a95cf5d01c217264
sergiusmuzzz <sergiorichie@gmail.com>
2021-02-22 00:00:44 +0300

    BACKEND - REST API, DTO

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import {Module} from "@nestjs/common";
+import {TrackModule} from "./track/track.module";
+import {MongooseModule} from "@nestjs/mongoose";
+
+@Module({
+    imports: [
+        MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.9iknz.mongodb.net/music-platform?retryWrites=true&w=majority'),
+        TrackModule
+    ]
+})
+export class AppModule {}
